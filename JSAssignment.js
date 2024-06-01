4fbcc87c29a72137ec04315f09f1726bc215367d
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-06-01 13:26:45 +0000

    Final Algo Code JAvaScript Assignment

--- JSAssignment.js
+++ JSAssignment.js
@@ -9,23 +9,36 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
-
+let nftCollection = [];
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT () {
-
+function mintNFT (id, name, eyeColor, jerseyType, biling) {
+    let newNFT ={
+        id: id,
+        name: name,
+        eyeColor: eyeColor,
+        jerseyType: jerseyType,
+        biling: biling,
+    }
+    nftCollection.push(newNFT);
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
-
+    for(let m = 0; m < nftCollection.length; m++){
+        let nft = nftCollection[m];
+        console.log("NFT ID: " + nft.id + " ,Name:" + nft.name + " ,Eye Color:" + nft.eyeColor + " ,Jersey Type:" + nft.jerseyType + " ,Biling:" + nft.biling) ;
+    }
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-
+    console.log("Total no of NFT: " + nftCollection.length);
 }
 
+mintNFT(1, "Rahul", "Black", "Simgle jursey", "dimand")
 // call your functions below this line
+listNFTs();
+getTotalSupply();
